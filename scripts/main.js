4ea85ab6ae8d53928a263e6335a1846d5b41188e
Emily Deans <emilyldeans@gmail.com>
2017-07-13 14:32:53 -0400

    Created a function that found the sum of the first hand.

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,27 @@
    Open up the `index.html` file and your console
    to watch the assertions pass as you write your code.
 
-   Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   Also remember, that the parameter `cards` will be an array, so
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
-function handValue (hand) {
+function handValue(cards) {
+  let totalValue = 0;
+
+  for (let i = 0; i < cards.length; i++) {
+      totalValue = parseInt(cards[i]) + totalValue;
+    }
+  //   cards[i] = parseInt(cards [i]);
+  // }
 
 
-  return;
+// loop over all cards and convert to total value
+// convert string value into number
+// add numerical value to total value
+
+  return totalValue;
 }
 
 
